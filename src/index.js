b4c9b0e3f42f99e718b1561831d375141934b14c
renatocuellar <matealva@gmail.com>
2021-07-06 23:08:35 -0500

    We add all assets and now we're ready to start building the components

--- src/index.js
+++ src/index.js
@@ -1,16 +1,8 @@
-// const element = document.createElement('h1');
-// element.innerText = 'Hello, Platzi Badges!';
-
-// const container = document.getElementById('app');
-
-// container.appendChild(element);
-
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const element = <h1>Hello, Platzi Badges!</h1>;
+const element = <h1>Hello, Eventopia from React!</h1>
 
 const container = document.getElementById('app');
 
-// ReactDOM.render(__qué__, __dónde__);
 ReactDOM.render(element, container);
