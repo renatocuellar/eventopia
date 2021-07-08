0facf39abd94bab4b7d4b9bdd62dfc84402781d7
renatocuellar <matealva@gmail.com>
2021-07-07 22:48:27 -0500

    We create our first component and start to use styles

--- src/Components/card.js
+++ src/Components/card.js
@@ -1,13 +1,16 @@
 import React from 'react';
 
+import "./styles/card.css";
+import eventLogo from '../Assets/Custom-Size–2.png';
+
 class Card extends React.Component {
     render() {
-        return <section>
-            <container>
-                <img src="" alt="Event Brand" />
+        return <section className="Card">
+            <container className="Card__header">
+                <img src={eventLogo} alt="Event Brand" />
             </container>
-            <container>
-                <img src="" alt="Avatar" />
+            <container className="Card__section-name">
+                <img className="Card__avatar" src="https://es.gravatar.com/gravatars/edit-rating/f2293506755dd8c1022d06b6b2355bf8" alt="Avatar" />
                 <h2>Renato <br/> Cuéllar</h2>
             </container>
             <container>
